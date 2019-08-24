6155afb95c402f5003d0d8fb1c316df34a85ea3f
joe <skyjoe0722@gmail.com>
2019-08-24 12:47:57 +0800

    1

---
+++ app/src/main/java/tw/brad/apps/bradb30/toastctivity.java
@@ -0,0 +1,14 @@
+package tw.brad.apps.bradb30;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class toastctivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_toastctivity);
+    }
+}
